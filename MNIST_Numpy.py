49d48039fe2699cecafe1310d8600c7aa3d2dde4
JP <joaopmgd@gmail.com>
2017-12-07 17:58:18 -0200

    Backpropagation

--- MNIST_Numpy.py
+++ MNIST_Numpy.py
@@ -59,6 +59,27 @@ def compute_cost(A, Y):
     cost = np.squeeze(cost)
     return cost
 
+def backward_propagation(parameters, cache, X, Y):
+    m = X.shape[0]
+    W1 = parameters["W1"]
+    W2 = parameters["W2"]
+    
+    A1 = cache["A1"]
+    A2 = cache["A2"]
+    
+    dZ2 = A2 - Y
+    dW2 = (1/m) * (np.dot(dZ2, A1.T))
+    db2 = (1/m) * (np.sum(dZ2, axis = 1, keepdims = True))
+    dZ1 = np.dot(W2.T, dZ2) * (1 - np.power(A1, 2))
+    dW1 = (1/m) * (np.dot(dZ1, X.T))
+    db1 = (1/m) * (np.sum(dZ1, axis = 1, keepdims = True))
+    
+    grads = {"dW1": dW1,
+             "db1": db1,
+             "dW2": dW2,
+             "db2": db2}
+    return grads
+
 def two_layer_model(X, Y, parameters, learning_rate = 0.001, num_iterations = 2):
     grads = {}
     costs = []
@@ -68,6 +89,8 @@ def two_layer_model(X, Y, parameters, learning_rate = 0.001, num_iterations = 2)
 
         A, cache = forward_propagation(X, parameters)
         cost = compute_cost(A, Y)
+
+        grads = backward_propagation(parameters, cache, X, Y)
         
         if i % 100 == 0:
             print("Cost after iteration {}: {}".format(i, np.squeeze(cost)))
